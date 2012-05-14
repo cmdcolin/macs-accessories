87d915b2e40f8a9ace89ae1102262df68b6ea72e
Colin <cplusnerd@gmail.com>
2012-05-14 13:16:51 -0600

    Functional changes to code

--- readplot.R
+++ readplot.R
@@ -19,88 +19,65 @@ for (i in files) {
 # Use MACS to find S96 peaks
 # macs14 --bw=25 --mfold=4,30 -g 1.2e7 -w -t chip -c control
 s96bed=read.table('S96/S96_peaks.bed')
-read=array()
-read2=array()
-for(i in 1:length(s96bed$V1))
-{
-  chr=s96bed$V1[i];
-  start=s96bed$V2[i];
-  end=s96bed$V3[i];
-                
-  # Reads from S96
-  wigfile=paste ('S96_treat_afterfiting_' , chr, '.wig.gz', sep='')
-  wig=get(wigfile)
-  peak=wig[wig$V1>start & wig$V1<end,];
-  totalreads=apply(t(peak$V2),1,sum) ;
-  read[i]=totalreads
-                
-  # reads for HS959
-  wigfile=paste('HS959_treat_afterfiting_',chr, '.wig.gz',sep='')
-  wig=get(wigfile)
-  peak2=wig[wig$V1>start & wig$V1<end,];
-  totalreads2=apply(t(peak2$V2),1,sum);
-  read2[i]=totalreads2
-} 
-
-
-plot(read,read2,ylim=c(0,30000),xlim=c(0,50000),ylab='HS959 reads',xlab='S96 reads',pch='*')
+read1=getS96Reads(s96bed)
+read2=getHS959Reads(s96bed)
+plot(read1,read2,ylim=c(0,30000),xlim=c(0,50000),ylab='HS959 reads',xlab='S96 reads',pch='*')
 title('S96 peaks')
 
 
 
-
+#####
 # Overlap of peaks in both HS959 and S96
 # intersectBed -a S96_peaks.bed -b HS959_peaks.bed -wa >> S96_overlap.bed
-s96bed=read.table('S96_overlap.bed')
-read3=array()
-read4=array()
-for(i in 1:length(s96bed$V1))
-{
-  chr=s96bed$V1[i];
-  start=s96bed$V2[i];
-  end=s96bed$V3[i];
-  
-  # Reads from S96
-  wigfile=paste('S96_treat_afterfiting_',chr,'.wig.gz',sep='')
-  wig=get(wigfile)
-  peak=wig[wig$V1>start & wig$V1<end,];
-  totalreads=apply(t(peak$V2),1,sum)
-  read3[i]=totalreads
-  
-  # reads for HS959
-  wigfile=paste('HS959_treat_afterfiting_',chr,'.wig.gz',sep='')
-  wig=get(wigfile)
-  peak2=wig[wig$V1>start & wig$V1<end,];
-  totalreads2=apply(t(peak2$V2),1,sum);
-  read4[i]=totalreads2
-}
+s96overlap=read.table('S96_overlap.bed')
+read3=getS96Reads(s96overlap)
+read4=getHS959Reads(s96overlap)
 points(read3,read4,pch=1,col='red')
 
 ##
 # Unique S96 peaks
 # subtractBed -a S96_peaks.bed -b S96_overlap.bed >> S96_unique.bed
-s96bed=read.table('S96_unique.bed')
-read5=array()
-read6=array()
-for(i in 1:length(s96bed$V1)) {
-  chr=s96bed$V1[i];
-  start=s96bed$V2[i];
-  end=s96bed$V3[i];
-  
-  # Reads from S96
-  wigfile=paste('S96_treat_afterfiting_',chr,'.wig.gz',sep='')
-  wig=get(wigfile)
-  peak=wig[wig$V1>start & wig$V1<end,];
-  totalreads=apply(t(peak$V2),1,sum);
-  read5[i]=totalreads
-  
-  #reads for HS959
-  wigfile=paste('HS959_treat_afterfiting_',chr,'.wig.gz',sep='')
-  wig=get(wigfile)
-  peak2=wig[wig$V1>start & wig$V1<end,];
-  totalreads2=apply(t(peak2$V2),1,sum);
-  read6[i]=totalreads2
-}
+s96unique=read.table('S96_unique.bed')
+read5=getS96Reads(s96unique)
+read6=getHS959Reads(s96unique)
 points(read5,read6,pch=1,col='green')
 
 
+
+############
+# Get total reads in S96 from bedfile
+getS96Reads<-function(bedfile) {
+  reads=array();
+  for(i in 1:length(bedfile$V1)) {
+    chr=bedfile$V1[i];
+    start=bedfile$V2[i];
+    end=bedfile$V3[i];
+    
+    # Reads from S96
+    wigfile=paste('S96_treat_afterfiting_',chr,'.wig.gz',sep='');
+    wig=get(wigfile);
+    peak=wig[wig$V1>start & wig$V1<end,];
+    totalreads=apply(t(peak$V2),1,sum);
+    reads[i]=totalreads;
+  }
+  reads
+}
+
+############
+# Get total reads in HS959 from bedfile
+getHS959Reads<-function(bedfile) {
+  reads=array();
+  for(i in 1:length(bedfile$V1)) {
+    chr=bedfile$V1[i];
+    start=bedfile$V2[i];
+    end=bedfile$V3[i];
+    
+    #reads for HS959
+    wigfile=paste('HS959_treat_afterfiting_',chr,'.wig.gz',sep='');
+    wig=get(wigfile);
+    peak2=wig[wig$V1>start & wig$V1<end,];
+    totalreads2=apply(t(peak2$V2),1,sum);
+    reads[i]=totalreads2;
+  }
+  reads
+}
