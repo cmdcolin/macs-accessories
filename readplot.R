cefba830ee3337e2a5c4719859966e50d036042c
Colin <cplusnerd@gmail.com>
2012-05-14 14:45:18 -0600

    HS959 includes

--- readplot.R
+++ readplot.R
@@ -12,7 +12,6 @@ for (i in files) {
   file<-paste('HS959/HS959_MACS_wiggle/treat/',i,sep='')
   x<-read.table(file, skip=2)
   assign(i,x)
-  print (i)
 }
 
 ####
@@ -27,20 +26,48 @@ title('S96 peaks')
 
 
 #####
-# Overlap of peaks in both HS959 and S96
-# intersectBed -a S96_peaks.bed -b HS959_peaks.bed -wa >> S96_overlap.bed
-s96overlap=read.table('S96_overlap.bed')
+# Overlap of peaks
+# intersectBed -a S96_peaks.bed -b HS959_peaks.bed -wa
+s96overlap=read.table('S96/S96_overlap.bed')
 read3=getS96Reads(s96overlap)
 read4=getHS959Reads(s96overlap)
 points(read3,read4,pch=1,col='red')
 
 ##
 # Unique S96 peaks
-# subtractBed -a S96_peaks.bed -b S96_overlap.bed >> S96_unique.bed
-s96unique=read.table('S96_unique.bed')
+# subtractBed -a S96_peaks.bed -b S96_overlap.bed
+s96unique=read.table('S96/S96_unique.bed')
 read5=getS96Reads(s96unique)
 read6=getHS959Reads(s96unique)
 points(read5,read6,pch=1,col='green')
+
+
+
+##
+# HS959 peaks
+hs959bed=read.table('HS959/HS959_peaks.bed')
+read7=getHS959Reads(hs959bed)
+read8=getS96Reads(hs959bed)
+plot(read7,read8,ylim=c(0,50000),xlim=c(0,30000),ylab='S96 reads',xlab='HS959 reads',pch='*')
+title('HS959 peaks')
+
+#####
+# Overlap of peaks
+# intersectBed -a HS959/HS959_peaks.bed -b S96/S96_peaks.bed -wa
+HS959overlap=read.table('HS959/HS959_overlap.bed')
+read9=getHS959Reads(HS959overlap)
+read10=getS96Reads(HS959overlap)
+points(read9,read10,pch=1,col='blue')
+
+##
+# Unique HS959 
+# subtractBed -a HS959/HS959_peaks.bed -b HS959/HS959_overlap.bed  > HS959/HS959_unique.bed
+
+hs959unique=read.table('HS959/HS959_unique.bed')
+read11=getHS959Reads(hs959unique)
+read12=getS96Reads(hs959unique)
+points(read11,read12,pch=1,col='green')
+
 
 
 
