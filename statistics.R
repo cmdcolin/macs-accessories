5dd200d38163f484e70a07dca0cc9df27efae0c7
tonto <diesh@colorado.edu>
2012-06-15 03:08:45 -0600

    Sorted NormDiff statistics

--- statistics.R
+++ statistics.R
@@ -31,3 +31,29 @@ plotSortedMaxAvgZscoreX('Sorted HS959 Max Avg Normdiff in S96 peak regions',wig1
 plotSortedMaxAvgZscoreX('Sorted S96 Normdiff in HS959 peak regions',wig2,wig1,r2,'green','blue')
 plotSortedMaxAvgZscoreX('Sorted S96 peak Avg NormDiff score vs HS959 synteny w=100', wig1, wig2, r3, 'red', 'darkblue')
 plotSortedMaxAvgZscoreX('Sorted HS959 peak Avg NormDiff score vs S96 synteny w=100', wig2, wig1, r4, 'green','darkblue')
+
+
+plot(r1[['2shared']],dnorm(r1[['2shared']]),pch='.',col='blue',xlim=c(-10,10),ylim=c(0,0.4))
+points(r1[['2unique']],dnorm(r1[['2unique']]),pch='.',col='red')
+
+plot(r1[['2shared']],dexp(r1[['2shared']]),pch='.',col='blue',xlim=c(-10,10),ylim=c(0,0.4))
+points(r1[['2unique']],dexp(r1[['2unique']]),pch='.',col='red')
+
+plot(r1[['2shared']],dt(r1[['2shared']],1),pch=1,col='blue',xlim=c(-10,10),ylim=c(0,0.4))
+points(r1[['2unique']],dt(r1[['2unique']],1),pch='.',col='red')
+plot(r1[['2shared']],pt(r1[['2shared']],10),pch='.',col='blue')
+points(r1[['2unique']],pt(r1[['2unique']],10),pch='.',col='red')
+       
+       
+       
+
+r2s=c(r1[['2shared']],r1[['2unique']])
+corr1=match(r1[['2shared']],r2s)
+corr2=match(r1[['2unique']],r2s)
+r2s=(r2s-mean(r2s))/sqrt(var(r2s))
+r2snorm=dnorm(r2s,mean(r2s),sqrt(var(r2s)))
+r2s1norm=dnorm(r1[['2shared']],mean(r2s),sqrt(var(r2s)))
+r2s2norm=dnorm(r1[['2unique']],mean(r2s),sqrt(var(r2s)))
+plot(r2s,r2snorm,pch='.',col='blue')
+points(r2s[corr1],r2snorm[corr1],pch=20,col='red')
+points(r2s[corr2],r2snorm[corr2],pch=20,col='green')
