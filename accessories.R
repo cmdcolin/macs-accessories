fb6e4ac8b43bf8c73cbd79625c4c9c7979a35f5f
Colin <cplusnerd@gmail.com>
2012-08-03 15:25:25 +0100

    Take NormDiff out of WiggleClass

---
+++ accessories.R
@@ -0,0 +1,86 @@
+#access
+
+
+
+nc$getChipReads <- function(bedfile) {
+  getChipReadsX<-function(x,filepath){
+    chr=x[1]
+    start=as.integer(x[2])
+    end=as.integer(x[3])
+    wigfile=paste(filepath,chr,'.wig.gz',sep='')
+    wig=nc$wiglist[[wigfile]]
+    corr=findInterval(seq(start,end,by=nc$spacing),wig$V1)
+    ret=wig$V2[corr]
+    if(debug)
+      cat(as.integer(end)-as.integer(start), ' ', ret, '\n')
+    ret
+  }
+  # Apply to chip
+  apply(bedfile,1,getChipReadsX,nc$treatname)
+}
+
+#######
+# Get avg reads
+nc$getTotalReads = function(bedfile) {
+  getTotalReads<-function(x,filepath){
+    chr=x[1]
+    start=x[2]
+    end=x[3]
+    wigfile=paste(filepath,chr,'.wig.gz',sep='')
+    wig=nc$wiglist[[wigfile]]
+    corr=findInterval(start:end,wig$V1)
+    ret=sum(wig$V2[corr])
+    if(debug)
+      cat(as.integer(end)-as.integer(start), ' ', ret, '\n')
+    ret
+  }
+  # Apply to chip
+  apply(bedfile,1,getTotalReads,nc$treatname)
+} 
+
+# Get avg reads
+nc$getAvgReads = function(bedfile) {
+  getAvgReads<-function(x, filepath)
+  {  
+    chr=x[1]
+    start=x[2]
+    end=x[3]
+    wigfile=paste(filepath,chr,'.wig.gz',sep='')
+    wig=nc$wiglist[[wigfile]]
+    corr=findInterval(start:end,wig$V1)
+    b=head(corr,1)
+    e=tail(corr,1)
+    sum(wig$V2[corr])/(e-b);
+  }
+  #Apply to treat data
+  apply(bedfile,1,getAvgReads,nc$treatname)
+}
+
+nc$getMaxAvgReads<-function(bedfile, window=100) {
+  # Get max average reads over window size
+  getMaxAvgReads<-function(x, filepath, window)
+  {
+    chr=x[1];
+    start=as.integer(x[2]);
+    end=as.integer(x[3]);
+    wigfile=paste(filepath,chr,'.wig.gz',sep='')
+    wig=nc$wiglist[[wigfile]];
+    bstart=start-window
+    bend=end
+    maxreads=sapply(seq(bstart,end,by=window),function(p){
+      b=p
+      e=p+window
+      corr=findInterval(b:e,wig$V1)
+      sum(wig$V2[corr])/window
+    })
+    ret=max(maxreads)
+    if(debug)
+      cat('Found max ',ret,' in ',chr,'\n');
+    ret
+  }
+  # Apply to treated data
+  apply(bedfile,1,getMaxAvgReads,filepath=nc$treatname,window)
+}
+
+
+
--- zscore.R
+++ zscore.R
@@ -104,12 +104,12 @@ Z<-function(wig, bedfile, scaling, variance, window=c(1,10)) {
 
 
 # Calculate Z scores over all wiggle files
-nc$Zall<-function(window=c(1,10)) {
+Zall<-function(wig, window=c(1,10)) {
   files1=list.files(path=nc$treatpath,pattern="*.fsa.wig.gz")
   files2=list.files(path=nc$controlpath,pattern="*.fsa.wig.gz")
   ret=apply(cbind(files1,files2),1,function(f){
-    treat<-nc$wiglist[[f[1]]]
-    control<-nc$wiglist[[f[2]]]
+    treat<-wig$wiglist[[f[1]]]
+    control<-wig$wiglist[[f[2]]]
     if(debug==TRUE)
       cat(f[1],'\t',f[2],'\n')
     
@@ -136,4 +136,56 @@ nc$Zall<-function(window=c(1,10)) {
   #  ret[i][['chr']]=chr
   #}
   #ret
+}
+
+
+
+
+
+getMaxAvgZscoreAll<-function(wza,ws=100) {
+  ret=data.frame(chr=character(0),start=numeric(0),end=numeric(0),normdiff=numeric(0))
+  for(z in wza) {
+    print
+    chr=z[,1]
+    tpos=z[,2]
+    cpos=z[,3]
+    scores=z[,4]
+    if(debug)
+      cat(length(scores),'\n')
+    for(i in seq(1,length(scores)-1,by=ws)) {
+      start=i
+      end=i+ws
+      mchr=chr[start]
+      mscore=mean(as.numeric(scores[start:end]))
+      row=data.frame(mchr,start,end,mscore)
+      ret<-rbind(ret,row)
+    }
+    if(debug)
+      cat(length(scores),' ',nrow(ret),'\n')
+  }
+  ret
+}
+
+
+getMaxAvgZscore<-function(wz,ws=10) {
+  sapply(wz,function(zlist){
+    
+    zlist=unlist(zlist)
+    len=length(zlist)
+    reads=numeric(len)
+    #if(debug)
+    #  print(len)
+    if(is.numeric(zlist)==FALSE) {
+      cat('here1 ', length(zlist), typeof(zlist),'\n')
+      #  print(unlist(zlist))
+      #  zlist=unlist(zlist)
+    }
+    
+    for(i in 1:len) {
+      b=i
+      e=i+ws
+      reads[i]=mean(zlist[b:e],na.rm=TRUE)
+    }
+    max(reads)
+  })
 }
