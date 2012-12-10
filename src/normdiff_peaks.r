fe4da908191dc05f402caaafe25367d071b2abbe
Colin <cplusnerd@gmail.com>
2012-12-10 16:19:10 -0700

    Hackathon stage 2

--- src/normdiff_peaks.r
+++ src/normdiff_peaks.r
@@ -48,18 +48,18 @@ nd2=ret[[4]]
 str=c('chr','start','end','score')
 names(nd1)<-str
 names(nd2)<-str
-plot(0,0,xlim=c(-3,15),ylim=c(-3,15))
+plot(0,0,xlim=c(-3,20),ylim=c(-3,20))
 chrs=levels(nd1$chr)
 results=lapply(chrs,function(name) {
   exchr1=nd1[nd1$chr==name,]
   exchr2=nd2[nd2$chr==name,]
   
-  intervals=findInterval(as.integer(exchr1$start),as.integer(exchr2$start),all.inside=TRUE)
-  len=length(intervals)
+]
+  len=length(exchr1$start)
   print(len)
-  x=sapply(intervals[seq(0,len-10,by=10)],function(i) {
-    pos=findInterval(i,intervals)
-    sel=intervals[pos:(pos+10)]
+  x=sapply(exchr1$start[seq(0,len-10,by=10)],function(i) {
+    pos=findInterval(i,exchr1$start)
+    sel=intervals[pos:(pos+100)]
     
     m1=mean(exchr1[sel,]$score)
     m2=mean(exchr2[sel,]$score)
@@ -70,17 +70,22 @@ results=lapply(chrs,function(name) {
 
 # gen all points
 chr1=results[[1]]
-plot(chr1[,1],chr1[,2],pch='.',xlab='HS959rep1',ylab='S96rep1')
+plot(chr1[,1],chr1[,2],pch='.',xlab='S96rep1',ylab='HS959rep1',xlim=c(-5,15),ylim=c(-5,13))
 for(i in 1:16) {
   chr=results[[i]]
-  points(chr[,1],chr[,2],pch=20,cex=0.9,col=rgb(0,0,0,0.1))
+  points(chr[,2],chr[,1],pch=20,cex=0.9,col=rgb(0,0,0,0.05))
 }
 
 
 
 # find intersections
-bed1=ret2[[1]]
-bed2=ret2[[4]]
+#bed1=ret2[[1]]
+#bed2=ret2[[4]]
+
+bed1=read.table('SHout.bed')
+bed2=read.table('SHunique.bed')5
+bed1=read.table('hSout.bed')
+bed2=read.table('Hsunique.bed')
 str=c('chr','start','end','name','score')
 names(bed1)<-str
 names(bed2)<-str
@@ -93,61 +98,45 @@ for(i in 1:16) {
   exbed2=bed2[bed2$chr==name,] 
   chr=results[[i]]
   list1=apply(exbed1,1,function(x){
-    max(sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
+    listy=sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
       
-      sel=int[int[p]:(int[p]+10)]
+      sel=findInterval(seq(p,p+100,by=10),exchr2[,2])
       mean(exchr1[sel,]$score)
-    }
-    ))})
+    })
+    n=length(listy)
+     sort(listy)[(n-1):n]
+})
   list2=apply(exbed2,1,function(x){
-    max(sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
+    listy=sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
       
-      sel=int[int[p]:(int[p]+10)]
+      sel=findInterval(seq(p,p+100,by=10),exchr2[,2])
       mean(exchr1[sel,]$score)
-    }
-    ))})
+    })
+    n=length(listy)
+    sort(listy)[(n-1):n]
+    })
 
 list3=apply(exbed1,1,function(x){
-  max(sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
+  listy=sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
     
-    sel=int[int[p]:(int[p]+10)]
+    sel=findInterval(seq(p,p+100,by=10),exchr2[,2])
     mean(exchr2[sel,]$score)
-  }
-  ))})
-list4=apply(exbed2,1,function(x){
-  max(sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
+  })
+  n=length(listy)
+   sort(listy)[(n-1):n]
+  })
+  list4=apply(exbed2,1,function(x){
+    listy=sapply(seq(as.integer(x[2]),as.integer(x[3]),10),function(p) {
     
-    sel=int[int[p]:(int[p]+10)]
-    mean(exchr2[sel,]$score)
-  }
-  ))})
-  intervals1=findInterval(list1,exchr1[,2])
-  intervals2=findInterval(list1,exchr2[,2])
-  intervals3=findInterval(list2,exchr1[,2])
-  intervals4=findInterval(list2,exchr2[,2])
-  int=intervals1
-  r1=sapply(1:length(intervals1),function(i) {
-    sel=int[int[i]:(int[i]+10)]
-    mean(exchr1[sel,]$score)
+      sel=findInterval(seq(p,p+100,by=10),exchr2[,2])
+      mean(exchr2[sel,]$score)
+    })
+    n=length(listy)
+     sort(listy)[(n-1):n]
   })
-  int=intervals2
-  r2=sapply(1:length(intervals2),function(i) {
-    sel=int[int[i]:(int[i]+10)]
-    mean(exchr1[sel,]$score)
-  })
-  points(max(r1),max(r2),pch=20,cex=0.9,col=rgb(1,0,0,0.2))
+  points(unlist(list1),unlist(list3),cex=0.9,col=rgb(1,0,0,0.5))
   
-  int=intervals3
-  r1=sapply(1:length(intervals3),function(i) {
-    sel=int[int[i]:(int[i]+10)]
-    mean(exchr2[sel,]$score)
-  })
-  int=intervals4
-  r2=sapply(1:length(intervals4),function(i) {
-    sel=int[int[i]:(int[i]+10)]
-    mean(exchr2[sel,]$score)
-  })
-  points(r1,r2,pch=20,cex=0.9,col=rgb(0,0,1,0.2))
+  points(unlist(list2),unlist(list4),cex=0.9,col=rgb(0,1,0,0.5))
 
 }
 
