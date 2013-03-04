6c9be7754283ea737195a2ef029eaecf492d08b7
Colin <cplusnerd@gmail.com>
2013-03-04 02:39:28 -0700

    Remove unnecessary code from analyzezscore.R

--- src/spring13/analyzezscore-main.R
+++ src/spring13/analyzezscore-main.R
@@ -8,40 +8,20 @@
 
 wiggles<-getscoresmod(chrnames,macswiggle[[1]]$treat,macswiggle[[1]]$control,macswiggle[[2]]$treat,macswiggle[[2]]$control)
 
-#load('wigglesdata.rda')
+
+load('wiggledata.rda')
 #wiggles=wiggles
 
 
 
-#no background subtraction
-plot(wiggles$treat1,wiggles$treat2,pch='.',xlab='S96 rep1 treated',ylab='S96 rep2 treated')
-title('S96 replicates ChIP-seq scores (no control)')
-
-
-
-
-
-# background subtraction
-plot(wiggles$treat1-wiggles$control1,wiggles$treat2-wiggles$control2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
-title('S96 replicates ChIP-seq scores (background subtract)')
-
-
-#background scaling
 m1=median(wiggles$control1/wiggles$treat1)
 m2=median(wiggles$control2/wiggles$treat2)
-l1<-wiggles$treat1-wiggles$control1/m1
-l2<-wiggles$treat2-wiggles$control2/m2
-plot(l1,l2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
-title('S96 replicates ChIP-seq scores (background scaling)')
 
+background1<-wiggles$treat1-wiggles$control1
+background2<-wiggles$treat2-wiggles$control2
 
-#normalization
-v1<-sqrt(mean(wiggles$treat1)+mean(wiggles$control1)/m1^2)
-v2<-sqrt(mean(wiggles$treat2)+mean(wiggles$control2)/m2^2)
-plot(l1/v1,l2/v2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
-title('S96 replicates ChIP-seq scores (normdiff w=all)')
-smoothScatter(l1/v1,l2/v2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
-title('S96 replicates ChIP-seq scores (normdiff w=all)')
+scale1<-wiggles$treat1-wiggles$control1/m1
+scale2<-wiggles$treat2-wiggles$control2/m2
 
 
 treatmeans<-slideMean(wiggles$treat1)
@@ -52,14 +32,47 @@ controlmeans<-slideMean(wiggles$control2)
 vlist2<-sqrt(treatmeans+controlmeans/m2^2)
 
 
-#normdiff local
+v1<-sqrt(mean(wiggles$treat1)+mean(wiggles$control1)/m1^2)
+v2<-sqrt(mean(wiggles$treat2)+mean(wiggles$control2)/m2^2)
 vlist1f<-sapply(vlist1,function(v,vall){max(v,vall)},v1)
 vlist2f<-sapply(vlist2,function(v,vall){max(v,vall)},v2)
 
-plot(l1/vlist1,l2/vlist2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
+normdiff1<-scale1/vlist1f
+normdiff2<-scale2/vlist2f
+
+#no background subtraction
+plot(wiggles$treat1,wiggles$treat2,pch='.',xlab='S96 rep1 treated',ylab='S96 rep2 treated')
+title('S96 replicates ChIP-seq scores (no control)')
+
+
+
+
+
+# background subtraction
+plot(background1,background2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
+title('S96 replicates ChIP-seq scores (background subtract)')
+
+
+#background scaling
+plot(scale1,scale2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
+title('S96 replicates ChIP-seq scores (background scaling)')
+
+
+#normalization
+plot(l1/v1,l2/v2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
+title('S96 replicates ChIP-seq scores (normdiff w=all)')
+smoothScatter(l1/v1,l2/v2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
+title('S96 replicates ChIP-seq scores (normdiff w=all)')
+
+
+
+
+#normdiff local
+
+plot(normdif1,normdiff2,pch='.',xlab='S96 rep1',ylab='S96 rep2')
 title('S96 replicates ChIP-seq scores (normdiff w=local)')
 
-smoothScatter(l1/vlist1,l2/vlist2,pch='.',xlab='S96 rep1',ylab='S96 rep2',ylim=c(-5,35),xlim=c(-5,35))
+smoothScatter(normdiff1,normdiff2,pch='.',xlab='S96 rep1',ylab='S96 rep2',ylim=c(-5,35),xlim=c(-5,35))
 title('S96 replicates ChIP-seq scores (normdiff w=local)')
 
 
@@ -76,10 +89,8 @@ title('S96 MA plot for replicates (raw scores)')
 
 
 #no background subtraction-MA plot
-l1<-wiggles$treat1-wiggles$control1
-l2<-wiggles$treat2-wiggles$control2
-A=(l1+l2)*1/2
-M=(l1-l2)
+A=(background1+background2)*1/2
+M=(background1-background2)
 plot(A,M,pch='.',xlab='M',ylab='A')
 title('S96 MA plot for replicates (background subtraction)')
 
