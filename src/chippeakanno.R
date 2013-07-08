65e1047638f819bddecef634deadf2677b13c74e
Colin <cplusnerd@gmail.com>
2013-07-07 20:18:53 -0600

    Add edgeR script with chippeakanno

---
+++ src/chippeakanno.R
@@ -0,0 +1,30 @@
+# library(ChIPpeakAnno)
+# library(VennDiagram)
+library(limma)
+library(edgeR)
+
+# bed1=read.table('SEG1rep1u_peaks.bed')
+# bed2=read.table('SEG1rep2u_peaks.bed')
+# bed3=read.table('SEG1rep3u_peaks.bed')
+# bed123=read.table('SEG1rep123_peaks.bed')
+# 
+# 
+# bed1rd <- BED2RangedData(bed1,header=FALSE)
+# bed2rd <- BED2RangedData(bed2,header=FALSE)
+# bed3rd <- BED2RangedData(bed3,header=FALSE)
+# bed123rd <- BED2RangedData(bed123,header=FALSE)
+
+
+
+group <- factor(c(1,1,1,2,2,2))
+y=DGEList(wiggleTable[,c(-1,-2)],group,lib.size=c(2218566,1292603,2334845,1106212,1300837,2538686))
+y <- calcNormFactors(y)
+y <- estimateCommonDisp(y)
+y <- estimateTagwiseDisp(y)
+et <- exactTest(y)
+topTags(et)
+
+
+
+
+
