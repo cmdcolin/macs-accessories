aeeb0fc52c8f0959ba850be702967c5c9eff016c
Colin <cplusnerd@gmail.com>
2013-07-16 18:02:02 -0600

    Match chip with input samples in doNCIS

--- src/doNCIS.R
+++ src/doNCIS.R
@@ -1,16 +1,40 @@
 
 #convert all eland_result to bed 
 system("for f in *eland_result.txt; do elandresult2bed $f $f.bed; done")
+
+#look for bed files
 bedfiles<-list.files(pattern="*.bed")
-files<-lapply(bedfiles,function(filename) {
+input.match<-str_match(bedfiles,"(.*)inputu_eland_result.txt.bed")
+switchcases<-complete.cases(input.match)
+input.match<-input.match[switchcases,]
+input.match
+chip.match<-bedfiles[!switchcases]
+chip.match
+
+
+#get matches of chip with input files
+matches<-lapply(input.match[,2],function(input.sample) {
+  match.samples(chip.match,input.sample)
+})
+names(matches)<-input.match[,2]
+
+match.samples(chip.match,input.match[1,2])
+match.samples<-function(chip.samples,input.sample) {
+  which(!is.na(str_match(chip.samples,input.sample)))
+}
+
+
+
+
+####### load chip files and calculate a top variance without normdiff
+
+bedfiles.reads<-lapply(bedfiles,function(filename) {
   read.BED(filename)
 })
 
-input.match<-str_match(names(files),"(.*)input.*")
-chip.match<-str_match(names(files),"(.*)ChIP*")
+chip.list<-lapply(bedfiles.reads,function(elt) bin.chip(elt,1000,zero.filter=F)$chip)
 
-####### load chip files and calculate a top variance without normdiff
-chip.list<-lapply(files,function(elt) bin.chip(elt,1000,zero.filter=F)$chip)
+#convert to frame
 minlen<-min(sapply(chip.list,length))
 lencols<-lapply(chip.list,function(x)x[1:minlen])
 chip.frame<-do.call(cbind,lencols)
